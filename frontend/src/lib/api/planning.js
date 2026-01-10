c72895ffe05534dd43f8c42105a3c47f11dae06a
Nacer <nacer@DESKTOP-0HLH985.mshome.net>
2026-01-10 10:37:50 +0100

    fixing api bug

--- frontend/src/lib/api/planning.js
+++
@@ -1,6 +0,0 @@
-import { apiRequest } from "./client";
-
-export const planningApi = {
-  getPublished: () => apiRequest("/api/planning/published", { method: "GET" })
-};
-
--- frontend/src/lib/api/user.js
+++
@@ -1,6 +0,0 @@
-import { apiRequest } from "./client";
-
-export const getMe = async () => {
-  return apiRequest("/api/me", { method: "GET" });
-};
-
